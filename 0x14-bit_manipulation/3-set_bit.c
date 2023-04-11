6f46f3bc08d6c96b6d8dc58b3fabfddde61a3fc2
Prom1234pro <promiseimadonmwinyi@gmail.com>
2023-04-11 01:26:30 -0400

    alx...

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,10 +11,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask = 1UL << index;
+	
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	unsigned long int mask = 1UL << index;
 	*n |= mask;
 
 	return (1);
